e4ee39053ef7d2bf1b345d45c38215739e0cf0c1
Ansh Shukla <shuklaansh96@gmail.com>
2020-02-13 19:19:34 -0800

    reset

--- server.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-
-const port = process.env.port || 3334;
-
-const server = app.listen(port, () => {
-	console.log('Listening at http://localhost:' + port);
-});
-
-server.on('error', console.error);
