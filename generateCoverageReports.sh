287e6c11177c26257745be22de332cb610d0919e
Ansh Shukla <shuklaansh96@gmail.com>
2020-04-02 20:18:16 -0700

    Remove Deprecated Script [skip-ci]

--- generateCoverageReports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Generate Server coverage reports
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=\"json,cobertura,lcov\" /p:Threshold=80
-mkdir -p TestResults/Server && mv Server.Tests/TestResults/* ./TestResults/Server
-
-# Generate ClientApp coverage reports
-cd ./ClientApp && npm t
-mkdir -p ../TestResults/ClientApp && mv coverage/* ../TestResults/ClientApp
