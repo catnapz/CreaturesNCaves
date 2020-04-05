e89d82cc60916dfd52783611b8e1c06a4b1391c2
Ansh Shukla <shuklaansh96@gmail.com>
2020-04-04 20:07:41 -0700

    Material UI

--- ClientApp/src/components/layout/layout.tsx
+++ ClientApp/src/components/layout/layout.tsx
@@ -1,9 +1,34 @@
-import * as React from 'react';
+import React from 'react';
 import { NavMenu } from './nav-menu';
+import {SideBar} from "./side-bar";
+import useTheme from "@material-ui/core/styles/useTheme";
+import {useStyles} from "./shared-styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-export const Layout = (props: { children?: React.ReactNode }) => (
-  <>
-    <NavMenu />
-    {props.children}
-  </>
-);
+export const Layout = (props: { children?: React.ReactNode }) => {
+  const [isDrawerOpen, setDrawerOpen] = React.useState(false);
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+  
+  return (
+    <>
+      <div className={classes.root}>
+        <CssBaseline/>
+        <NavMenu handleMenuButtonClick={handleDrawerOpen} isDrawerOpen={isDrawerOpen}/>
+        <SideBar handleDrawerClose={handleDrawerClose} isDrawerOpen={isDrawerOpen}/>
+        <main className={classes.content}>
+          <div className={classes.toolbar}/>
+          {props.children}
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ ClientApp/src/components/layout/side-bar.tsx
@@ -0,0 +1,81 @@
+﻿import React from 'react';
+import clsx from 'clsx';
+import {Drawer} from "@material-ui/core";
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import useTheme from "@material-ui/core/styles/useTheme";
+import {useStyles} from "./shared-styles";
+import Typography from "@material-ui/core/Typography";
+import {NavLink} from "react-router-dom";
+import "./side-bar.scss";
+
+interface SidebarProps {
+  handleDrawerClose: () => void;
+  isDrawerOpen: boolean;
+}
+
+export const SideBar = (props: SidebarProps) => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  
+  return (
+    <Drawer
+      variant="permanent"
+      id="side-bar"
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: props.isDrawerOpen,
+        [classes.drawerClose]: !props.isDrawerOpen,
+      })}
+      classes={{
+        paper: clsx({
+          [classes.drawerOpen]: props.isDrawerOpen,
+          [classes.drawerClose]: !props.isDrawerOpen,
+        }),
+      }}
+    >
+      <div className={classes.toolbar}>
+        <IconButton onClick={props.handleDrawerClose}>
+          {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
+      </div>
+        <Typography variant="h5" className={clsx('sidebar-category-text', {
+          'visible': props.isDrawerOpen
+        })} >Campaign</Typography>
+      <Divider/>
+      <List>
+        <ListItem button>
+          <ListItemIcon><InboxIcon/></ListItemIcon>
+          <ListItemText primary="Campaigns" />
+        </ListItem>
+      </List>
+      <Typography variant="h5" className={clsx('sidebar-category-text', {
+        'visible': props.isDrawerOpen
+      })} >Utility</Typography>
+      <Divider/>
+      <List>
+        
+        <NavLink className="sidebar-nav-link" to="/counter">
+          <ListItem button className="sidebar-nav-list-item">
+            <ListItemIcon className="sidebar-nav-list-item-icon"><InboxIcon/></ListItemIcon>
+            <ListItemText className="sidebar-nav-list-item-text" primary="Counter" />
+          </ListItem>
+        </NavLink>
+
+        <NavLink className="sidebar-nav-link" to="/checkusers">
+          <ListItem button className="sidebar-nav-list-item">
+            <ListItemIcon className="sidebar-nav-list-item-icon"><InboxIcon/></ListItemIcon>
+            <ListItemText className="sidebar-nav-list-item-text" primary="Users" />
+          </ListItem>
+        </NavLink>
+        
+      </List>
+    </Drawer>
+  );
+}
