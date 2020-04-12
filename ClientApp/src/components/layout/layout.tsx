7f6b71c6b44e8f766259cb9d6a2c2cc9ae4e7c15
Ansh Shukla <shuklaansh96@gmail.com>
2020-04-12 15:40:28 -0700

    side bar category

--- ClientApp/src/components/layout/layout.tsx
+++ ClientApp/src/components/layout/layout.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavMenu } from './nav-menu';
-import {SideBar} from "./side-bar";
+import { SideBar } from "./side-bar";
 import useTheme from "@material-ui/core/styles/useTheme";
-import {useStyles} from "./shared-styles";
+import { useStyles } from "./shared-styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 export const Layout = (props: { children?: React.ReactNode }) => {
   const [isDrawerOpen, setDrawerOpen] = React.useState(false);
   const theme = useTheme();
   const classes = useStyles(theme);
-  
+
   const handleDrawerOpen = () => {
     setDrawerOpen(true);
   };
@@ -17,15 +17,15 @@ export const Layout = (props: { children?: React.ReactNode }) => {
   const handleDrawerClose = () => {
     setDrawerOpen(false);
   };
-  
+
   return (
     <>
       <div className={classes.root}>
-        <CssBaseline/>
-        <NavMenu handleMenuButtonClick={handleDrawerOpen} isDrawerOpen={isDrawerOpen}/>
-        <SideBar handleDrawerClose={handleDrawerClose} isDrawerOpen={isDrawerOpen}/>
+        <CssBaseline />
+        <NavMenu handleMenuButtonClick={handleDrawerOpen} isDrawerOpen={isDrawerOpen} />
+        <SideBar handleDrawerClose={handleDrawerClose} isDrawerOpen={isDrawerOpen} />
         <main className={classes.content}>
-          <div className={classes.toolbar}/>
+          <div className={classes.toolbar} />
           {props.children}
         </main>
       </div>
---
+++ ClientApp/src/components/layout/side-bar-category.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { cleanup, render, wait, getByText } from '@testing-library/react';
+import { SideBarCategory } from "./side-bar-category";
+
+describe("CreateCampaignMenu", () => {
+
+  afterAll(() => {
+    cleanup();
+  });
+
+  it('should render successfully', async () => {
+    const { baseElement } = render(<SideBarCategory open={false} title={"Test Category Title"}/>);
+
+    await wait(() => getByText(baseElement, "Test Category Title"));
+    
+  });
+
+  it('should contain visible className when open', async () => {
+    const { baseElement } = render(<SideBarCategory open={true} title={"Test Category Title"}/>);
+
+    await wait(() => getByText(baseElement, (content, element) => {
+      return element.classList.contains("visible") && content === "Test Category Title";
+    }));
+    
+  });
+
+  
+});
