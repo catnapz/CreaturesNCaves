1480df9f13028248c4fd2df09d1ea1e92356952d
Ansh Shukla <contact@anshshukla.dev>
2021-01-24 01:25:51 -0800

    Update unit test

--- ClientApp/src/components/layout/nav-menu/nav-menu.test.tsx
+++ ClientApp/src/components/layout/nav-menu/nav-menu.test.tsx
@@ -7,7 +7,7 @@ import * as authStore from "../../pages/user/auth/auth-store.slice";
 
 import { NavMenu } from "./nav-menu";
 
-jest.mock('../../user/logout/logout-dialog', () => ({
+jest.mock('../../pages/user/logout/logout-dialog', () => ({
   LogoutDialog: 'mocked-logout-dialog'
 }));
 
